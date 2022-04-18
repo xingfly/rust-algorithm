a0f468fb7055d1b1c548a9d8d20d9046dd7a8f54
SuperS <549052145@qq.com>
2022-04-18 17:09:24 +0800

    03 - 基于队列的特性解决烫手山芋问题

--- chapter1/src/base/queue.rs
+++ chapter1/src/base/queue.rs
@@ -36,7 +36,7 @@ impl<T> Queue<T> {
         0 == Self::size(&self)
     }
 }
-
+// 队列基础使用
 pub fn queue_test_1() {
     let mut queue = Queue::new(3);
     let _r1 = queue.enqueue(1);
@@ -55,4 +55,28 @@ pub fn queue_test_1() {
     }
 
     println!("content:{:?}", queue);
+}
+
+// 烫手山芋问题
+pub fn queue_test_2() {
+    let names = vec!["1", "2", "3", "4", "5", "6"];
+    let rem = hot_potato(names, 8);
+    println!("最后剩下：{rem}");
+}
+
+fn hot_potato(names: Vec<&str>, num: usize) -> &str {
+    let mut queue = Queue::new(names.len());
+    for name in names {
+        let _ = queue.enqueue(name);
+    }
+    while queue.size() > 1 {
+        // 在num没有达到前，每个人从队首出队，再重新入队到队尾
+        for _i in 0..num {
+            let name = queue.dequeue().unwrap();
+            let _rm = queue.enqueue(name);
+        }
+        // 到达num后，队首的人出队
+        let _rm = queue.dequeue();
+    }
+    queue.dequeue().unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use chapter1::base::{queue::queue_test_1, stack::stack_test_4};
+use chapter1::base::{
+    queue::{queue_test_1, queue_test_2},
+    stack::stack_test_4,
+};
 fn main() {
     // stack_test_1();
     // stack_test_2();
     // stack_test_3();
     // stack_test_4();
-    queue_test_1();
+    queue_test_2();
 }
